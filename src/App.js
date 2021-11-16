e574b27c1dc630dcb3dc675f18be6bfac3429a83
happychimzy <89752017+happychimzy@users.noreply.github.com>
2021-11-16 13:37:35 +0100

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import {Route, Routes } from 'react-router';
+import {BrowserRouter as Router, } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Characters from './components/Characters';
+import Planets from './components/Planets';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className = "px-5">
+        <h1 className = "fw-bold text-dark"> Spacious</h1>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Planets />} />
+          <Route exact path ='/character' element={<Characters/>}/>
+          </Routes>
+      </div>
+
+      </Router>
+      
   );
+  
 }
 
+
+
 export default App;
+
